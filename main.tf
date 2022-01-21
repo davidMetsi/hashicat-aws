f526b91c7db4098f45417811be2fb4396233b901
davidMetsi <david.joy@metsi.co>
2022-01-21 23:17:53 +0000

    Added s3 bucket module

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_vpc" "hashicat" {
 
 module "s3-bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "2.13.0"
+  # version = "2.13.0"
   # insert the 5 required variables here
   bucket_prefix = "david-joy"
   
